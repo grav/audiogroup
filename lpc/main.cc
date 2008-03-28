0fd8ed36f5cb913b1fcce7c86c4c5e9d22a6b1ce
deva <deva@example.com>
2008-03-28 12:45:38 +0000

    File input and output are now defned on commandline.

--- lpc/main.cc
+++ lpc/main.cc
@@ -1,33 +1,42 @@
 #include <sndfile.hh>
 #include "lpc.h"
 
-#define WINDOW_SIZE 100
+#include <math.h>
+
+#define WINDOW_SIZE (int)(1.7 * 882) // 882 frames = 20ms
 #define COEFS 32
 
-int main()
+int main(int argc, char *argv[])
 {
-	char ifilename[] = "input.wav";
-	char ofilename[] = "output.wav";
+  if(argc < 3) {
+    printf("Usage: %s [intput] [output]\n", argv[0]);
+    return 1;
+  }
 
-	lpc_data lpc = lpc_create();
+  char *ifilename = argv[1];
+  char *ofilename = argv[2];
 
-	SAMPLE x[WINDOW_SIZE];
-	SAMPLE y[WINDOW_SIZE];
-	float coefs[COEFS];
-	float power;
-	float pitch;
+  lpc_data lpc = lpc_create();
 
+  SAMPLE x[WINDOW_SIZE];
+  SAMPLE y[WINDOW_SIZE];
+  float coefs[COEFS];
+  float power;
+  float pitch;
 
-	SndfileHandle isfh(ifilename);
-	SndfileHandle osfh(ofilename, SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, 1, 44100);
-	
-	while(isfh.read(x, WINDOW_SIZE) != 0) {
-		lpc_analyze(lpc, x, WINDOW_SIZE, coefs, COEFS, &power, &pitch);
-		lpc_synthesize(lpc, y, WINDOW_SIZE, coefs, COEFS, power, pitch);
-		osfh.write(y, WINDOW_SIZE);
-	}
+  SndfileHandle isfh(ifilename);
+  SndfileHandle osfh(ofilename, SFM_WRITE, SF_FORMAT_WAV | SF_FORMAT_PCM_16, 1, 44100);
 
-	lpc_destroy(lpc);
+  int cnt = 0;
+  while(isfh.read(x, WINDOW_SIZE) != 0) {
+    lpc_analyze(lpc, x, WINDOW_SIZE, coefs, COEFS, &power, &pitch);
+    lpc_synthesize(lpc, y, WINDOW_SIZE, coefs, COEFS, power * 2, pitch);
+    printf("pitch %f\n", pitch);
+    osfh.write(y, WINDOW_SIZE);
+    cnt++;
+  }
+  
+  lpc_destroy(lpc);
 
-	return 0;
+  return 0;
 }
