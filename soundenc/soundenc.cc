252e559c88b752dae97348cb93f647e0dea1e751
deva <deva@example.com>
2008-06-10 07:20:46 +0000

    Removed invalid include

--- soundenc/soundenc.cc
+++ soundenc/soundenc.cc
@@ -24,8 +24,6 @@
  *  along with DSPToolBox; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
  */
-#include "soundenc.h"
-
 #include "wavread.h"
 #include "wavplay.h"
 #include "conv.h"
