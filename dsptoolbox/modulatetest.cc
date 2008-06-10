046597b2e40d6b5790f45cdfa9f9ee6108811cf0
deva <deva@example.com>
2008-06-10 19:06:24 +0000

    Modulation test program.

---
+++ dsptoolbox/modulatetest.cc
@@ -0,0 +1,53 @@
+/* -*- Mode: C++; tab-width: 2; indent-tabs-mode: nil; c-basic-offset: 2 -*- */
+/***************************************************************************
+ *            modulatetest.cc
+ *
+ *  Tue Jun 10 19:18:21 CEST 2008
+ *  Copyright 2008 Bent Bisballe Nyeng
+ *  deva@aasimon.org
+ ****************************************************************************/
+
+/*
+ *  This file is part of DSPToolBox.
+ *
+ *  DSPToolBox is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  DSPToolBox is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with DSPToolBox; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA.
+ */
+#include "samples.h"
+#include "modulate.h"
+#include "wavread.h"
+#include "wavplay.h"
+#include <cmath>
+/*
+sample_t mod_func(sample_t a)
+{
+  return a;// - 1000;
+}
+*/
+int main(int argc, char *argv[])
+{
+  samplerate_t xfs;
+  samples_t *x = wavread(argv[1], &xfs);
+
+  samples_t *y = modulate(x, 40);
+  samples_t *z = modulate(y, -40);
+
+  wavplay(x, xfs);
+  wavplay(y, xfs);
+  wavplay(z, xfs);
+  wavplay(x, xfs);
+  delete x;
+  delete y;
+  delete z;
+}
