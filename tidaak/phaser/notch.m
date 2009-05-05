dcc9bde803476b1a9090f6170568018dc4f34dcf
Mikkel Gravgaard <mikkel@klokke.dk>
2009-05-05 13:57:33 +0000

    phaser effect

---
+++ tidaak/phaser/notch.m
@@ -0,0 +1,50 @@
+function y = notch(x,notchFreq,A)
+
+N = length(x);
+
+% can't get global vars to work :-(
+fs = 44100;
+
+y = zeros(1,N);
+
+%% calculate initial a and b
+BW     = 1200;  % Bandwidth
+Apass  = 1;     % Bandwidth Attenuation
+[b, a] = iirnotch(notchFreq/(fs/2), BW/(fs/2), Apass);
+
+% just copy two first samples since we're using 3 coeffs.
+y(1)=x(1);
+y(2)=x(2);
+
+for n=3:N
+
+    % changing second a- and b-coeff. changes notch freq.
+    % thus we use our lfo controlled pShift to change it
+    % slightly up and down as a function of n
+    phase = 0;
+    lfoFreq = 2; % Hz
+    pShift = A * sin(2 * pi * n * lfoFreq/fs + phase);
+
+    b(2) = b(2)+pShift;
+    a(2) = a(2)+pShift;
+    
+    % make iir-filtering of current sample
+    % ref: http://en.wikipedia.org/wiki/IIR
+    bsum = 0;
+    for i=1:length(b)
+        bsum = bsum + b(i) * x(n-i+1);
+    end
+    
+    asum = 0;
+    for j=2:length(a)
+        asum = asum + a(j) * y(n-j+1);
+    end
+    y(n)= 1/a(1) * (bsum - asum);
+    
+    % change coeff back
+    b(2) = b(2)-pShift;
+    a(2) = a(2)-pShift;
+    
+end
+
+
---
+++ tidaak/phaser/phaser.m
@@ -0,0 +1,34 @@
+% oevelse 2.3
+% phaser
+% x: input signal 
+% y: processed signal
+% 
+
+%% init
+clear
+close all
+
+%% load sound
+fs=44100; % must also be changed in notch.m since I have no clue about global variables
+x=wavread('strings1.wav');
+x=x';
+
+%% impulse response
+%x = zeros(1,1000); x(1)=1;
+
+%% filter while changing 2nd coefficient of a and b
+y=x;
+
+% frequences to notch
+notchFreqs=[300    1200 3000 6000 10000  12000 14000 18000];
+
+% lfo amplifications
+notchAmps =[.0015 .025  .17   .65   1.5  1.6     1.0  .1];
+
+for i=1:length(notchFreqs)
+    y=notch(y,notchFreqs(i),notchAmps(i));
+   % imp = notch(imp,notchFreqs(i),notchAmps(i));
+end
+
+
+
