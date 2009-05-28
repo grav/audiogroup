0be265108ca7ea714efab1489be2f0c8cf4b039a
Mikkel Gravgaard <mikkel@klokke.dk>
2009-05-28 15:33:25 +0000

    somethings working

--- tidaak/phaser/phaser2.m
+++ tidaak/phaser/phaser2.m
@@ -2,7 +2,7 @@
 clear
 
 % define lfo frequency
-lfo = .5; % Hz
+lfo = 2; % Hz
 
 % define filter frequencies
 % todo: calculate these?
@@ -22,7 +22,7 @@ y = zeros(1,N);
 
 %% go girl
 
-fclog = zeros(1,N);
+fclog = zeros(length(fcs),N);
 
 y(1) = x(1); y(2) = x(2);
 for n = 3:N
@@ -30,29 +30,32 @@ for n = 3:N
   for m=1:length(fcs);
     % calculate filter parameters
     fc = fcs(m);
-    fc = fc + fc/10 * sin(2 * pi * n * lfo/fs);
+    fc = fc + fc/2 * sin(2 * pi * n * lfo/fs);
     fb = fc/10;
   
-    if m == 4 
-      fclog(n)=fc;
-    end
+    fclog(m,n)=fc;
     
     % calculate filter coefficients
     [b,a] = allpass2ndorder(fc,fb,fs);
     %fvtool(b,a);
-  end
   
-  % iir filtering
-  for i=1:length(b)
-    y(n) = b(i) * x(n-i+1);
-  end
-  for j=2:length(a)
-    y(n) = y(n) - a(i) * y(n-j+1);
-  end
-  y(n) = y(n)/a(1);
+    % iir filtering
+    for i=1:length(b)
+      y(n) = y(n) + b(i) * x(n-i+1);
+    end
+    for j=2:length(a)
+      y(n) = y(n) - a(i) * y(n-j+1);
+    end
+    y(n) = y(n)/a(1);
   
-  % mix with input
-%  y(n) = x(n)/2+y(n)/2;
+    % mix with input
+  %  y(n) = x(n)/2+y(n)/2;
+  end
   
 end
 
+figure
+hold on
+for i=1:length(fcs)
+  plot(fclog(i,:));
+end
