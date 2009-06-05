c98e234a5c28803e504ab971abd307f60eeb83b4
Mikkel Gravgaard <mikkel@klokke.dk>
2009-06-05 12:13:38 +0000

    before refactorization

--- tidaak/phaser/phaser.m
+++ tidaak/phaser/phaser.m
@@ -14,14 +14,14 @@ x=wavread('../sound/guitar1.wav');
 x=x';
 
 %% impulse response
-%x = zeros(1,1000); x(1)=1;
+x = zeros(1,1000); x(1)=1;
 
 %% filter while changing 2nd coefficient of a and b
 y=x;
 
 % frequences to notch
 notchFreqs=[300    1200 3000 6000 10000  12000 14000 18000];
-
+notchFreqs=[3000];
 % lfo amplifications
 notchAmps =[.0015 .025  .17   .65   1.5  1.6     1.0  .1];
 
@@ -30,5 +30,7 @@ for i=1:length(notchFreqs)
    % imp = notch(imp,notchFreqs(i),notchAmps(i));
 end
 
-play(y);
+plot(angle(fft(y)));
+
+% play(y);
 
